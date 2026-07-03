8497237d61a42011f5c2949e8d1bb87f6899d355
dody-z <dodydaudsinaga@gmail.com>
2026-07-03 09:49:13 +0700

    Submit Quiz 2

---
+++ quiz/part3/soal5.js
@@ -0,0 +1,15 @@
+//*
+//**
+//***
+//****
+//*****
+
+for (let i = 1; i <= 5; i+=1){
+    let bintang = "";
+
+    for (let j = 1; j <= i; j+=1){
+        bintang +="*";
+    }
+    console.log(bintang);
+    
+}
